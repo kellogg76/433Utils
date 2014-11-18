c49a682b36b77bb455b6073a6193a1ac20195689
kellogg76 <mathew.nightingale@gmail.com>
2014-11-18 09:00:27 -0400

    Update RFSniffer.cpp

--- RPi_utils/RFSniffer.cpp
+++ RPi_utils/RFSniffer.cpp
@@ -41,6 +41,7 @@ int main(int argc, char *argv[]) {
           printf("Received %i\n", mySwitch.getReceivedValue() );
           printf("matty");
         }
+        
         mySwitch.resetAvailable();
     
       }
